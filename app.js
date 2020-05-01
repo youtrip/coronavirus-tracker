17cdf22a0f705a4c7882be1061d9cdb2ae74198d
zl <youtrip@139.com>
2020-05-01 21:22:48 +0800

    增加热点地区，并增加确诊数和死亡数的趋势图标

--- app.js
+++ app.js
@@ -4,23 +4,85 @@ window.onload = function () {
 
 function getCovidStats(id) {
     fetch('https://coronavirus-tracker-api.herokuapp.com/v2/locations/' + id)
-    .then(function (resp) {
-        return resp.json()
-    }).then(function (data) {
-        let population = data.location.country_population;
-        let update = data.location.last_updated;
-        let confirmedCases = data.location.latest.confirmed;
-        let deaths = data.location.latest.deaths;
-        let us = data.location.country + " - " + data.location.province;
+        .then(function (resp) {
+            return resp.json()
+        }).then(function (data) {
+            let population = data.location.country_population;
+            let update = data.location.last_updated;
+            let confirmedCases = data.location.latest.confirmed;
+            let deaths = data.location.latest.deaths;
+            let us = data.location.country + " - " + data.location.province;
+            let hisConfirmedTimelines = data.location.timelines.confirmed.timeline;
+            let hisDeathsTimelines = data.location.timelines.deaths.timeline;
 
-        document.getElementById('population').innerHTML = population.toLocaleString('en');
-        document.getElementById('country').innerHTML = us.toLocaleString('en');
-        document.getElementById('update').innerHTML = update.substr(0, 10);
-        document.getElementById('cases').innerHTML = confirmedCases.toLocaleString('en');
-        document.getElementById('deaths').innerHTML = deaths.toLocaleString('en');
-        document.getElementById('percent').innerHTML = ((Number(deaths)/Number(confirmedCases))*100).toLocaleString('en', {minimumFractionDigits:2, maximumFractionDigits:2}) + "%";
-    }).catch(function () {
-        console.log("error");
-    })
-    setTimeout(getCovidStats, 3600) // update every 1h
+            let arrayConfirmedTime = [];
+            let arrayConfirmedCounter = [];
+
+            let arrayDeathsTime = [];
+            let arrayDeathsCounter = [];
+
+            for (let k in hisConfirmedTimelines){
+                arrayConfirmedTime.push(k.substr(5,5));
+                arrayConfirmedCounter.push(hisConfirmedTimelines[k]);
+            }
+
+            for (let k in hisDeathsTimelines){
+                arrayDeathsTime.push(k.substr(5,5));
+                arrayDeathsCounter.push(hisDeathsTimelines[k]);
+            }
+            console.log(arrayDeathsTime);
+            console.log(arrayDeathsCounter);
+
+            let ctx = document.getElementById('ConfirmedChart').getContext('2d');
+            const ConfirmedChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: arrayConfirmedTime,
+                    datasets: [{
+                        label: 'Confirmed',
+                        data: arrayConfirmedCounter,
+                        fill: false,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+            });
+
+            let ctx2 = document.getElementById('DeathsChart').getContext('2d');
+            const DeathsChart = new Chart(ctx2, {
+                type: 'line',
+                data: {
+                    labels: arrayDeathsTime,
+                    datasets: [{
+                        label: 'Deaths',
+                        data: arrayDeathsCounter,
+                        fill: false,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+            });
+
+
+            document.getElementById('population').innerHTML = population.toLocaleString('en');
+            document.getElementById('country').innerHTML = us.toLocaleString('en');
+            document.getElementById('update').innerHTML = update.substr(0, 10);
+            document.getElementById('cases').innerHTML = confirmedCases.toLocaleString('en');
+            document.getElementById('deaths').innerHTML = deaths.toLocaleString('en');
+            document.getElementById('percent').innerHTML = ((Number(deaths) / Number(confirmedCases)) * 100).toLocaleString('en', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) + "%";
+        }).catch(error => {
+            console.log('error!');
+            console.error("error");
+        })
+
 }
