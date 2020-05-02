3a3973f31970bc044b07f9b286cf7593cdfa3e16
zl <youtrip@139.com>
2020-05-02 23:59:56 +0800

    增加可选择全球所有国家和地区的功能，并可以新增和删除地区

--- app.js
+++ app.js
@@ -1,7 +1,174 @@
+const sessionKey = 'covid_location';
+const buttonStyle = 'btn-warning';
+const sessionLocationIDKey = 'covid_location_id';
+let baseLocalJson = {
+    225: '美国',
+    131: '印度',
+    120:'德国',
+    116: '法国',
+    137: '意大利',
+    196: '新加坡',
+    187: '俄罗斯',
+    200: '南非',
+    205: '瑞典',
+    139: '日本',
+    201: '西班牙',
+    223: '英国',
+};
+
 window.onload = function () {
-    getCovidStats(225)
+    setAddLocationsSelect();
+    setLacationButton();
+    getCovidStats(225);
+};
+
+//取所有地区ID，增加到下拉框选项中
+function setAddLocationsSelect() {
+    let ids = getAllLocationID();
+    for(let id in ids){
+        addOptionList(id,ids[id]);
+    }
 }
 
+//填充地区按钮
+//从本地缓存取自定义地区列表，没有则用默认地区列表，最后填充到html中。
+function setLacationButton() {
+
+    let location = sessionStorage.getItem(sessionKey);
+    if (location == null){
+        sessionStorage.setItem(sessionKey, JSON.stringify(baseLocalJson));
+        location = sessionStorage.getItem(sessionKey);
+    }
+
+    let localJson = JSON.parse(location);
+    for (let id in localJson){
+        newBotton(id,localJson);
+    }
+}
+
+//增加到下拉框option选项中。
+function addOptionList(id,name) {
+    let selectList = document.getElementById('selectCountryID');
+    let option = document.createElement('option');
+    option.setAttribute('value', id);
+    option.innerText = name;
+    selectList.appendChild(option);
+}
+
+//恢复到默认地区按钮列表。并强制浏览器刷新。
+function resetLocation() {
+    sessionStorage.removeItem(sessionKey);
+    sessionStorage.removeItem(sessionLocationIDKey);
+    window.location.reload();
+}
+
+//从接口获取所有的地区ID和名称，并缓存到本地。
+//返回所有地区id的json数据。
+function getAllLocationID() {
+    let ids = sessionStorage.getItem(sessionLocationIDKey);
+    if (ids==null){
+        let allID = {};
+        fetch('https://coronavirus-tracker-api.herokuapp.com/v2/locations?source=jhu&timelines=false')
+            .then(function (resp) {
+                return resp.json()
+            }).then(function (data) {
+            let name ='';
+            for(let key in data.locations){
+                if (data.locations[key].province ==''){
+                    name = data.locations[key].country + '(' + data.locations[key].country_code + ')';
+                }else{
+                    name = data.locations[key].country + '(' + data.locations[key].country_code + ')-' + data.locations[key].province;
+                }
+                allID[data.locations[key].id] = name;
+            }
+            sessionStorage.setItem(sessionLocationIDKey, JSON.stringify(allID));
+
+        }).catch(error => {
+            console.log('error!');
+            console.error("error");
+        });
+        return allID;
+    }
+    return JSON.parse(ids);
+
+}
+
+//新增地区按钮组
+function newBotton(id,localJson) {
+    let buttonRowGroup = document.getElementById('buttonGroup');
+    let buttonGroup = document.createElement('div');
+    buttonGroup.setAttribute('id','location-'+id);
+    buttonGroup.classList.add('btn-group');
+
+    let localButton = document.createElement('button');
+    localButton.setAttribute('onclick', 'getCovidStats('+id+')');
+    localButton.setAttribute('type','button');
+    localButton.classList.add('btn');
+    localButton.classList.add(buttonStyle);
+    localButton.innerText = localJson[id];
+    buttonGroup.appendChild(localButton);
+
+    let localButtonCaret = document.createElement('button');
+    localButtonCaret.setAttribute('type','button');
+    localButtonCaret.setAttribute('data-toggle','dropdown');
+    localButtonCaret.setAttribute('aria-expanded','false');
+    localButtonCaret.setAttribute('aria-haspopup','true');
+    localButtonCaret.classList.add('btn');
+    localButtonCaret.classList.add(buttonStyle);
+    localButtonCaret.classList.add('dropdown-toggle');
+    buttonGroup.appendChild(localButtonCaret);
+
+    let spanCaret = document.createElement('span');
+    spanCaret.classList.add('caret');
+    localButtonCaret.appendChild(spanCaret);
+
+    let ul = document.createElement('ul');
+    ul.classList.add('dropdown-menu');
+    buttonGroup.appendChild(ul);
+
+    let li = document.createElement('li');
+    ul.appendChild(li);
+
+    let link = document.createElement('a');
+    link.setAttribute('href','#');
+    link.setAttribute('onclick', 'deleteLocation('+id+')');
+
+    link.innerText = '删除';
+    li.appendChild(link);
+
+    buttonRowGroup.appendChild(buttonGroup);
+}
+
+// 删除指定ID的地区按钮
+function deleteLocation(id) {
+    let location = sessionStorage.getItem(sessionKey);
+    let localJson = JSON.parse(location);
+    delete localJson[id];
+    sessionStorage.setItem(sessionKey, JSON.stringify(localJson));
+
+    let delLocation = document.getElementById('location-'+id);
+    delLocation.classList.add('hidden');
+}
+
+// 增加地区按钮
+function addLocation() {
+    let location = sessionStorage.getItem(sessionKey);
+    let newid = document.getElementById('selectCountryID');
+    let newname = document.getElementById('inputName');
+    let localJson = JSON.parse(location);
+    let id = newid.value;
+    let name = newname.value;
+    if (id !=='' && name !==''){
+        let location = sessionStorage.getItem(sessionKey);
+        localJson[id] = name;
+        sessionStorage.setItem(sessionKey, JSON.stringify(localJson));
+        newBotton(id,localJson);
+    } else if(id === '-1'){
+        alert('请选择正确的国家或地区！')
+    }
+}
+
+// 获取接口指定地区的数据，并填充到html中。
 function getCovidStats(id) {
     fetch('https://coronavirus-tracker-api.herokuapp.com/v2/locations/' + id)
         .then(function (resp) {
@@ -87,4 +254,10 @@ function getCovidStats(id) {
             console.error("error");
         })
 
+}
+
+function pushToInput() {
+    let newid = document.getElementById('selectCountryID');
+    let inputLocation = document.getElementById('inputName');
+    // inputLocation.value =
 }
